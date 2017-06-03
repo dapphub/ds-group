a15c78913981adafe4e25d1b1a0b24c86d6680da
Daniel Brockman <daniel@brockman.se>
2017-06-03 17:05:47 +0200

    rewrite: backward compatibility + CLI

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-This repository contains `DSMultisig80`, a multisig implementation
-with no external dependencies, and a factory `DSMultisig80Factory`.
+Overview
+========
+
+This package contains an m-of-n multisig contract as well as a
+command-line interface for working with such multisig instances.
+
+Please join <https://dapphub.chat/> for more information.
+
+
+Command-line interface
+======================
+
+The `ds-group(1)` program is a convenient way to work with groups.
+
+To install the command-line program, type `make link`.  You need to
+have Seth installed to use it (<https://github.com/dapphub/seth>).
+
+
+Synopsis
+--------
+
+    Usage: ds-group <command> <group> [<args>]
+       or: ds-group <command> --help
+
+    Propose, confirm and trigger DSGroup multisig actions.
+
+    Commands:
+
+       action        print information about a multisig action
+       confirm       confirm a proposed multisig action
+       ls            list already-proposed multisig actions
+       propose       propose a new multisig action
+       trigger       trigger a confirmed multisig action
+       verify        verify the meaning of a multisig action
+
+
+Examples
+--------
+
+    ~$ ds-group ls @mkrgroup
+        ACT  CONFIRMATIONS      EXPIRATION  STATUS
+         15   0/6 (need 4)        8 h left  Unconfirmed
+         16   0/6 (need 4)        9 h left  Unconfirmed
+
+    ~$ ds-group propose @mkrgroup @feedbase 0 "claim()"
+    Proposing action...
+      target     0x5927c5cc723c4486f93bf90bad3be8831139499e
+      value      0
+      calldata   0x4e71d92d
+    seth-send: 0x307b667c434794c234b7c463b26827bdceb9c838fdb306f3f4398edefa5b1310
+    seth-send: Waiting for transaction receipt.........................
+    seth-send: Transaction included in block 1519991.
+    seth-send: note: return value may be inaccurate (see `seth send --help')
+    Successfully proposed act 17.
+
+    ~$ ds-group ls @mkrgroup
+        ACT  CONFIRMATIONS      EXPIRATION  STATUS
+         15   0/6 (need 4)        8 h left  Unconfirmed
+         16   0/6 (need 4)        9 h left  Unconfirmed
+         17   0/6 (need 4)       23 h left  Unconfirmed
+
+    ~$ ds-group confirm @mkrgroup 17
+    Confirming action 17...
+    seth-send: 0x72fc6bf7c5135645a0fa298aa3ae01e072a82eabfddc8e3fbcdca72d0007d94b
+    seth-send: Waiting for transaction receipt...............
+    seth-send: Transaction included in block 1520018.
+
+    ~$ ds-group ls @mkrgroup
+     ACTION  CONFIRMATIONS      EXPIRATION  STATUS
+         15   0/6 (need 4)        8 h left  Unconfirmed
+         16   0/6 (need 4)        9 h left  Unconfirmed
+         17   1/6 (need 4)       23 h left  Unconfirmed
+
+    ~$ ds-group trigger @mkrgroup 17
+    ds-group-trigger: error: act not confirmed: 17
+
+    ~$ ds-group action @mkrgroup 17
+    calldata        0x4e71d92d
+    confirmations   1
+    confirmed       false
+    deadline        1471876934
+    expired         false
+    status          Unconfirmed
+    target          0x5927c5cc723c4486f93bf90bad3be8831139499e
+    triggered       false
+    value           0
---
+++ src/group.t.sol
@@ -0,0 +1,259 @@
+/// group.t.sol -- tests for group.sol
+
+// Copyright (C) 2015, 2016  Ryan Casey <ryepdx@gmail.com>
+// Copyright (C) 2016, 2017  Daniel Brockman <daniel@brockman.se>
+
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
+
+pragma solidity ^0.4.11;
+
+import "ds-test/test.sol";
+
+import "./group.sol";
+
+contract DSGroupTest is DSTest {
+    DSGroupFactory  factory;
+    DSGroup         group;
+    Dummy           dummy;
+    Person          alice;
+    Person          bob;
+    Person          eve;
+    bytes           calldata;
+
+    function setUp() {
+        factory = new DSGroupFactory();
+        dummy = new Dummy();
+        alice = new Person();
+        bob = new Person();
+        eve = new Person();
+
+        address[] memory members = new address[](3);
+        members[0] = alice;
+        members[1] = bob;
+        members[2] = this;
+
+        group = factory.newGroup(members, 2, 3 days);
+    }
+
+    function test_setup() {
+        assertEq(group.members(0), alice);
+        assertEq(group.members(1), bob);
+        assertEq(group.members(2), this);
+
+        assert(group.isMember(this));
+        assert(group.isMember(alice));
+        assert(group.isMember(bob));
+        assert(!group.isMember(eve));
+
+        assertEq(group.quorum(), 2);
+        assertEq(group.memberCount(), 3);
+        assertEq(group.window(), 3 days);
+        assertEq(group.actionCount(), 0);
+
+        var (quorum, memberCount, window, actionCount) = group.getInfo();
+        assertEq(quorum, 2);
+        assertEq(memberCount, 3);
+        assertEq(window, 3 days);
+        assertEq(actionCount, 0);
+    }
+
+    function testFail_unconfirmed() {
+        var id = group.propose(dummy, new bytes(0), 0);
+        group.trigger(id);
+    }
+
+    function testFail_transfer() {
+        var id = group.propose(dummy, new bytes(0), 123);
+        group.confirm(id);
+        bob.confirm(group, id);
+        group.trigger(id);
+    }
+
+    function test_transfer() {
+        group.deposit.value(123)();
+        var id = group.propose(dummy, new bytes(0), 123);
+        group.confirm(id);
+        bob.confirm(group, id);
+        group.trigger(id);
+        assertEq(dummy.balance, 123);
+    }
+
+    function test_propose() {
+        assertEq(group.propose(dummy, new bytes(0), 0), 1);
+        assertEq(group.actionCount(), 1);
+        assertEq(group.target(1), dummy);
+        assertEq(group.value(1), 0);
+        assertEq(group.deadline(1), now + 3 days);
+        assertEq(group.confirmations(1), 0);
+        assert(!group.triggered(1));
+        assert(!group.expired(1));
+        assert(!group.confirmed(1));
+        assert(!group.confirmedBy(1, this));
+        assert(!group.confirmedBy(1, alice));
+        assert(!group.confirmedBy(1, bob));
+        assert(!group.confirmedBy(1, eve));
+    }
+
+    function test_second_propose() {
+        assertEq(group.propose(dummy, new bytes(0), 0), 1);
+        assertEq(group.propose(alice, new bytes(0), 0), 2);
+        assertEq(group.actionCount(), 2);
+        assertEq(group.target(1), dummy);
+        assertEq(group.target(2), alice);
+    }
+
+    function test_confirm() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        assertEq(group.confirmations(1), 1);
+        assert(group.confirmedBy(1, this));
+        assert(!group.confirmedBy(1, alice));
+        assert(!group.confirmedBy(1, bob));
+        assert(!group.confirmedBy(1, eve));
+        assert(!group.confirmed(1));
+        assert(!group.triggered(1));
+    }
+
+    function test_second_confirm() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        alice.confirm(group, 1);
+        assertEq(group.confirmations(1), 2);
+        assert(group.confirmedBy(1, this));
+        assert(group.confirmedBy(1, alice));
+        assert(group.confirmed(1));
+        assert(!group.triggered(1));
+    }
+
+    function test_third_confirm() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        alice.confirm(group, 1);
+        bob.confirm(group, 1);
+        assertEq(group.confirmations(1), 3);
+        assert(group.confirmedBy(1, bob));
+        assert(group.confirmed(1));
+        assert(!group.triggered(1));
+    }
+
+    function testFail_double_confirm() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        group.confirm(1);
+    }
+
+    function testFail_unauthorized_confirm() {
+        group.propose(dummy, new bytes(0), 0);
+        eve.confirm(group, 1);
+    }
+
+    function testFail_premature_trigger() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        group.trigger(1);
+    }
+
+    function test_trigger() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        alice.confirm(group, 1);
+        assert(!dummy.fallbackCalled());
+        group.trigger(1);
+        assert(group.triggered(1));
+        assert(dummy.fallbackCalled());
+    }
+
+    function test_failed_trigger() {
+        group.propose(dummy, new bytes(0), 0);
+        group.confirm(1);
+        alice.confirm(group, 1);
+        dummy.setFallbackBlocked(true);
+        assert(!group.call(bytes4(sha3("trigger(uint256)")), 1));
+        assert(!group.triggered(1));
+        assert(!dummy.fallbackCalled());
+    }
+
+    function test_payment() {
+        group.deposit.value(500)();
+        assertEq(group.balance, 500);
+        assertEq(dummy.balance, 0);
+
+        group.propose(dummy, new bytes(0), 70);
+        group.confirm(1);
+        alice.confirm(group, 1);
+        group.trigger(1);
+        assertEq(dummy.balance, 70);
+        assertEq(alice.balance, 0);
+        assertEq(group.balance, 430);
+
+        group.propose(alice, new bytes(0), 430);
+        group.confirm(2);
+        alice.confirm(group, 2);
+        group.trigger(2);
+        assertEq(dummy.balance, 70);
+        assertEq(alice.balance, 430);
+        assertEq(group.balance, 0);
+    }
+
+    function testFail_payment() {
+        assert(group.call.value(500)());
+        group.propose(dummy, new bytes(0), 501);
+        group.confirm(0);
+        alice.confirm(group, 0);
+        group.trigger(0);
+    }
+
+    function test_calldata() {
+        bytes memory calldata = new bytes(4 + 32);
+        bytes4 sig = bytes4(sha3("foo(uint256)"));
+
+        calldata[0] = sig[0];
+        calldata[1] = sig[1];
+        calldata[2] = sig[2];
+        calldata[3] = sig[3];
+        calldata[4 + 31] = 123;
+
+        group.propose(dummy, calldata, 0);
+        group.confirm(1);
+        alice.confirm(group, 1);
+        group.trigger(1);
+        assertEq(dummy.fooArgument(), 123);
+    }
+}
+
+contract Dummy {
+    uint  public  fooArgument;
+    uint  public  fooValue;
+    bool  public  fallbackBlocked;
+    bool  public  fallbackCalled;
+
+    function () payable {
+        if (fallbackBlocked) {
+            throw;
+        } else {
+            fallbackCalled = true;
+        }
+    }
+
+    function setFallbackBlocked(bool yes) {
+        fallbackBlocked = yes;
+    }
+
+    function foo(uint argument) payable {
+        fooArgument  = argument;
+        fooValue     = msg.value;
+    }
+}
+
+contract Person {
+    function () payable {}
+
+    function confirm(DSGroup group, uint id) {
+        group.confirm(id);
+    }
+}
